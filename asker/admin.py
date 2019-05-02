aa58bc82f24ae5bdad5827e6dd8e1ac978a1d1d7
Андронов Дмитрий <andronovdima@MacBook-Pro-Andronovdima.local>
2019-05-02 16:14:24 +0300

    spare version

--- asker/admin.py
+++ asker/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from asker.models import Question,Profile , Answers
+from asker.models import Question,Profile , Answer , Tag
 
 
 # Register your models here.
 
 admin.site.register(Question)
+admin.site.register (Profile)
+admin.site.register (Answer)
+admin.site.register (Tag)
---
+++ asker/forms.py
@@ -0,0 +1,28 @@
+# from django import forms
+# from django.contrib import auth
+#
+# # #django crispy forms
+# # #django-widget-tweaks
+# #
+# #
+# class LoginForm(forms.Form):
+#     username = forms.CharField()
+#     password1 = forms.CharField(widget = forms.PasswordInput)
+#     password2 = forms.CharField(widget = forms.PasswordInput)
+#
+#
+#
+#     def clean(self):
+#         cleaned_data = super().clean()
+#             passone = cleaned_data.get('password1')
+#             passtwo = cleaned_data.get('password2')
+#         if cleaned_data
+#             raise forms.ValidationError('')
+
+#
+#     def cleanauth (self):
+#
+
+
+
+
